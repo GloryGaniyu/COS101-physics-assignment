661f97b91f88ad9f13c868ab7d380de8ee511e47
GloryGaniyu <glory.ganiyu@binghamuni.edu.ng>
2024-11-18 16:49:49 +0100

    Add files via upload

---
+++ MY_ASSIGNMENT.py
@@ -0,0 +1,23 @@
+
+def a(distance,time):
+    speed =distance/time
+    print(f'the speed is {speed}m/s^2')
+a(200,20)
+def b(mass,acceleration):
+    force =mass*acceleration
+    print(f'the force is {force}N')
+b(20,5)
+def c(length,breadth,height):
+    volume =length*breadth*height
+    print(f'the volume is {volume}m^3')
+c(12,4,6)
+def d(mass,volume):
+    density =mass/volume
+    print(f'the density is {density}kgm^-3')
+d(10000,200)
+def e(force,distance):
+    work =force*distance
+    print(f'the work is {work}Nm')
+e(200,50)
+#fuction a: Calculate Speed(v=d/t)
+
